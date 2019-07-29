29d4373e655e87ca3f1aea86b76b0823d461d600
chunglop0781 <chunglop0781@gmail.com>
2019-07-29 15:38:19 +0800

    Update vtvcab51.php

--- vtvcab51.php
+++ vtvcab51.php
@@ -1,1 +1,1 @@
-537030560476_high/index.m3u8
+https://vsd8.mycdn.me/hls/537030560476.m3u8/sig/-E450f-koRo/expires/1564472044736/srcIp/103.68.251.116/clientType/1/srcAg/CHROME_ANDROID_TABLET/537030560476_high/index.m3u8
