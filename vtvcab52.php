cae780b61509bec414db02609ed721059bf96af9
chunglop0781 <chunglop0781@gmail.com>
2019-08-01 10:29:56 +0800

    Update vtvcab52.php

--- vtvcab52.php
+++ vtvcab52.php
@@ -1,1 +1,1 @@
-https://vsd0.mycdn.me/hls/537030560476.m3u8/sig/sTjKMD8tp0A/expires/1564628695882/srcIp/103.68.251.116/clientType/1/srcAg/CHROME_ANDROID_TABLET/537030560476_low/index.m3u8
+https://vsd4.mycdn.me/hls/537030560476.m3u8/sig/kpDkilP4Aqg/expires/1564712933204/srcIp/103.68.251.116/clientType/1/srcAg/CHROME_ANDROID_TABLET/
